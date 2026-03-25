43b9dbfc7dd288bddfe0819c6d0289600dce3a48
deivis-raposo <84677245+deivis-raposo@users.noreply.github.com>
2026-03-25 11:22:50 -0300

    Ajuste na estrutura e layout v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "./pages/LoginPage";
-import { DashboardPage } from "./pages/DashboardPage";
-import { LoggedOutPage } from "./pages/LoggedOutPage";
-import { ProtectedRoute } from "./auth/ProtectedRoute";
-import { useAuth } from "./auth/useAuth";
+import { AppRoutes } from "./routes/AppRoutes";
 
-function AppRoutes() {
-  const { isAuthenticated } = useAuth();
-
-  return (
-    <Routes>
-      <Route
-        path="/login"
-        element={isAuthenticated ? <Navigate to="/" replace /> : <LoginPage />}
-      />
-
-      <Route path="/logged-out" element={<LoggedOutPage />} />
-
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <DashboardPage />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-  );
+function App() {
+  return <AppRoutes />;
 }
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
-}
+export default App;
